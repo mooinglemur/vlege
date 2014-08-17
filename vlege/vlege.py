6d61599f2d537ed1e5d183a1c777a1381b63c964
Eugene E. Kashpureff Jr <eugene@kashpureff.org>
2014-08-17 19:57:39 +0000

    Add verbosity control, logging facility

--- vlege/vlege.py
+++ vlege/vlege.py
@@ -6,8 +6,11 @@ import os
 # Image-handling
 from PIL import Image
 # Argument-handling
+import argparse
+# Logging
+import logging
 
-## Variable setup
+## Define constants
 # Initialize empty array
 size={}
 # Size of thumbnails
@@ -15,11 +18,29 @@ size["thumb"]=(200,200)
 # Size of intermediates
 size["medium"]=(1000,1000)
 
+## Argument parsing
+# Initialize
+parser = argparse.ArgumentParser()
+# Enumerate args
+parser.add_argument("-v", "--verbose", help="Increase output verbosity",
+                    action="store_true")
+# Annnd... parse!
+args = parser.parse_args()
 
+## Output & Logging
+if args.verbose:
+    # Verbose logging
+    logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.DEBUG)
+    logging.info("Verbose output.")
+else:
+    # Standard logging
+    logging.basicConfig(format="%(levelname)s: %(message)s")
+
+## Main Loop
 # Walk filesystem hierarchy
 for root, dirs, files in os.walk("."):
     # Current directory
-    print("Directory: ", root)
+    logging.info("Directory: "+root)
     # Walk through files
     for filename in files:
         # Full path to file
@@ -29,24 +50,24 @@ for root, dirs, files in os.walk("."):
         # Basic name of file
         filebase=filepath+filesplit[0]
         # Echo
-        print("File: "+filepath+filename)
+        logging.info("File: "+filepath+filename)
         # Skip non-images
         if filesplit[1].lower() != ".jpg":
-            print("Not a jpg!")
+            logging.info("Skipped, not a jpg")
             continue
         # Skip already-processed
         if os.path.isfile(filebase+"-thumb.jpg"):
-            print("Thumb found!")
+            logging.info("Skipped, thumb found")
             continue
         # Only process originals
         if "-medium" in filesplit[0] or "-thumb" in filesplit[0]:
-            print("Not an original!")
+            logging.info("Skipped, is a thumb/med")
             continue
         # Load file
         try:
             img = Image.open(filepath+filename)
         except IOError:
-            print("Could not open file!")
+            logging.error("Could not open file")
             continue
         # Create+save a Thumbnail
         thumb = img.copy()
@@ -54,7 +75,7 @@ for root, dirs, files in os.walk("."):
             thumb.thumbnail(size["thumb"])
             thumb.save(filebase+"-thumb.jpg")
         except IOError:
-            print("Could not create thumb!")
+            logging.error("Could not create thumb")
         # Do we need a Medium?
         if img.size > size["medium"]:
             # Create+savea Medium
@@ -63,5 +84,5 @@ for root, dirs, files in os.walk("."):
                 medium.thumbnail(size["medium"])
                 medium.save(filebase+"-medium.jpg")
             except IOError:
-                print("Could not create medium!")
+                logging.error("Could not create medium")
 
