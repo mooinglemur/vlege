588be73b943475a822889676e531b08ada784753
Eugene E. Kashpureff Jr <eugene@kashpureff.org>
2015-03-13 05:02:54 +0000

    Pass PEP8 checks, mostly.

--- vlege/vlege.py
+++ vlege/vlege.py
@@ -16,7 +16,8 @@
 
 ## Dependencies
 # Basics
-import os, sys
+import os
+import sys
 # Logging
 import logging
 # CLI friendliness
@@ -26,8 +27,8 @@ from PIL import Image, ImageOps
 # Output templating
 import jinja2
 
-## Function definitions
 
+## Function definitions
 def vlege(path=None, thumb=200, dryrun=False):
     """Vlege-ize a path
 
@@ -45,14 +46,13 @@ def vlege(path=None, thumb=200, dryrun=False):
 
     # Size of thumb
     size={}
-    size["thumb"]=(thumb,thumb)
+    size["thumb"]=(thumb, thumb)
 
     ## Initialize Jinja
     # Initialize Jinja
-    JINJA = jinja2.Environment(
-        loader=jinja2.FileSystemLoader(os.path.dirname(os.path.realpath(__file__))))
-    index_template = JINJA.get_template('index.html.jinja')
-
+    JINJA=jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(
+        os.path.realpath(__file__))))
+    index_template=JINJA.get_template('index.html.jinja')
 
     ## Directory processing loop
     # Walk filesystem hierarchy
@@ -63,8 +63,8 @@ def vlege(path=None, thumb=200, dryrun=False):
         dir_name=os.path.basename(root)
 
         # Reset for this directory
-        folders = []
-        images = []
+        folders=[]
+        images=[]
 
         ## Subdirectory processing loop
         dirs.sort()
@@ -85,7 +85,7 @@ def vlege(path=None, thumb=200, dryrun=False):
             # Information
             logging.info("File: "+filename)
             # Skip non-images
-            if file_ext != ".jpg":
+            if file_ext !=".jpg":
                 logging.info("Skipped, not a jpg")
                 continue
             # Only process originals
@@ -94,8 +94,7 @@ def vlege(path=None, thumb=200, dryrun=False):
                 continue
             # Add to list
             images.append({'file': filename,
-                'thumb': file_base+'-thumb.jpg'
-            })
+                          'thumb': file_base+'-thumb.jpg'})
             # Skip already-processed
             if os.path.isfile(dir_path+file_base+"-thumb.jpg"):
                 logging.info("Skipped, thumb found")
@@ -105,35 +104,34 @@ def vlege(path=None, thumb=200, dryrun=False):
                 continue
             # Load file
             try:
-                img = Image.open(dir_path+filename)
+                img=Image.open(dir_path+filename)
             except IOError:
                 logging.error("Could not open file "+dir_path+filename)
                 continue
             # Create+save a Thumbnail
             try:
-                thumb = ImageOps.fit(img,size["thumb"], method=Image.ANTIALIAS)
+                thumb=ImageOps.fit(img, size["thumb"],
+                                   method=Image.ANTIALIAS)
                 thumb.save(dir_path+file_base+"-thumb.jpg")
             except IOError:
-                logging.error("Could not create thumb "+dir_path+file_base+"-thumb.jpg")
+                logging.error("Could not create thumb " + dir_path+file_base +
+                              "-thumb.jpg")
 
         ## Index file building
         # Album information
-        logging.info("Building index for "+root)
-        index_values = {
-            'album': { 'title': dir_name,
-                'separator': '-',
-                'path': root
-            },
-            'folders': folders,
-            'images': images
-        }
+        logging.info("Building index for " + root)
+        index_value={'album': {'title': dir_name,
+                               'separator': '-',
+                               'path': root},
+                     'folders': folders,
+                     'images': images}
         # Generate index
-        index_rendered = index_template.render(index_values)
+        index_rendered=index_template.render(index_values)
         # Skip on dry runs
         if dryrun is True:
             continue
         # Open+truncate index file
-        index_file = open(root+'/index.html', 'w')
+        index_file=open(root+'/index.html', 'w')
         # Write out contents
         index_file.write(index_rendered)
         # Done with file
@@ -143,22 +141,23 @@ def vlege(path=None, thumb=200, dryrun=False):
 def main():
     ## Argument parsing
     # Initialize
-    parser = argparse.ArgumentParser("vlege")
+    parser=argparse.ArgumentParser("vlege")
 
     # Enumerate args
     parser.add_argument("-d", "--dryrun", action="store_true",
-                            help="Perform a dry run(do not modify files)")
+                        help="Perform a dry run(do not modify files)")
     parser.add_argument("-v", "--verbose", action="store_true",
-                            help="Increase output verbosity")
+                        help="Increase output verbosity")
     parser.add_argument("album", help="Path to an album to be processed")
 
     # Annnd... parse!
-    args = parser.parse_args()
+    args=parser.parse_args()
 
     # Set up logging
     if args.verbose:
         # Verbose logging
-        logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.DEBUG)
+        logging.basicConfig(format="%(levelname)s: %(message)s",
+                            level=logging.DEBUG)
         logging.info("Verbose output enabled")
     else:
         # Standard logging
